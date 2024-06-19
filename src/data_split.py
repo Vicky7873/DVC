14a619ed26daaca2d8471ad0f3ba521c66026b10
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-06-20 02:21:41 +0530

    for repro

--- src/data_split.py
+++ src/data_split.py
@@ -1,26 +1,29 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+import pandas as pd 
+from sklearn.model_selection import train_test_split 
 import argparse
-import yaml
 import os
+import yaml
+import numpy as np
 
 def data_split(param_yaml_path):
-    with open(param_yaml_path) as yaml_file:
-        param_yaml = yaml.safe_load(yaml_file)
+    # with open(param_yaml_path) as yaml_file:
+    #     param_yaml = yaml.safe_load(yaml_file)
+    param_yaml = yaml.safe_load(open(param_yaml_path))
     local_data = param_yaml["data_source"]["local_path"]
-
+    
     data_df = pd.read_csv(local_data)
-    data_df['quality'] = np.where(data_df['quality']>6.5,1,0)
+    data_df['quality'] = np.where(data_df['quality']>6.5, 1,0)
     random_state = param_yaml["base"]["random_state"]
     split_ratio = param_yaml["split"]["split_ratio"]
-
-    train, test =train_test_split(data_df,
-                                  random_state=random_state,
-                                  test_size=split_ratio)
     
-    os.makedirs(param_yaml["split"]["dir"],exist_ok=True)
-    train_data_path = os.path.join(param_yaml["split"]["dir"],param_yaml["split"]["train_file"])
+    train, test = train_test_split(
+        data_df, 
+        test_size=split_ratio, 
+        random_state=random_state
+        )
+    os.makedirs(param_yaml["split"]["dir"], exist_ok=True)
+    train_data_path = os.path.join(param_yaml["split"]["dir"], param_yaml["split"]["train_file"])
+    
     train.to_csv(train_data_path, index=False)
 
     test_data_path = os.path.join(param_yaml["split"]["dir"], param_yaml["split"]["test_file"])
